a7a386d64ab846914c76f51c287e0bd1a052fcbe
crcox <cox.crc@gmail.com>
2014-05-01 14:04:59 -0500

    Fixed logic of removing used voxels.
    Also made sure that structured arrays work.

--- main.m
+++ main.m
@@ -4,8 +4,9 @@
 function results = main()
 	%% Set Variables
 	% For you, it will be something like ../data
-	DATA_PATH = '/home/chris/JLPeacock_JNeuro2008/orig/mat';
-	N_SUB = 10;
+% 	DATA_PATH = '/home/chris/JLPeacock_JNeuro2008/orig/mat';
+    DATA_PATH = '../data';
+% 	N_SUB = 10;
 	N_CV = 10;
 	% Just an estimate for the max number of iterations, for preallocating.
 	N_ITER_EST = 10; 
@@ -25,7 +26,7 @@ function results = main()
 	CVBLOCKS = CVBLOCKS{1};
 	Y = Y{1};
 
-	% Before starting the loop: see if there is a checkpoint file.
+	%% Before starting the loop: see if there is a checkpoint file.
 	if exist(fullfile(pwd, 'CHECKPOINT.mat'), 'file') == 2
 		load('CHECKPOINT.mat');
 		start_cc = cc;
@@ -37,11 +38,12 @@ function results = main()
 		start_cc = 1;
 		dp = zeros(N_ITER_EST,N_CV);
 		err = zeros(N_ITER_EST,N_CV);
-		fitObj(N_ITER_EST,N_CV) = struct();
-		fitObj_cv(N_ITER_EST,N_CV) = struct();
+		fitObj = init_glmnet_result_struct('glmnet',[N_ITER_EST, N_CV]);
+        fitObj_cv = init_glmnet_result_struct('cvglmnet',[N_ITER_EST, N_CV]);
         UNUSED_VOXELS = true(size(X,2),N_CV,N_ITER_EST);
     end
 	
+    %% Start the loop
 	while true
 		% Increment loop counter
 		ii = ii + 1;
@@ -66,11 +68,11 @@ function results = main()
 			% Run cvglmnet to determine a good lambda.
 			fitObj_cv(ii,cc) = cvglmnet(Xtrain(:,UNUSED_VOXELS(:,cc,ii)),Ytrain, ...
 				                     'binomial',opts,'class',9,fold_id);
-
+            
 			% Set that lambda in the opts structure, and fit a new model.
 			opts.lambda = fitObj_cv(ii,cc).lambda_min;
-			fitObj(ii,cc) = glmnet(Xtrain(:,UNUSED_VOXELS(:,cc,ii)),Ytrain,'binomial',opts);
-
+  			fitObj(ii,cc) = glmnet(Xtrain(:,UNUSED_VOXELS(:,cc,ii)),Ytrain,'binomial',opts);
+            
 			% Unset lambda, so next time around cvglmnet will look for lambda
 			% itself.
 			opts = rmfield(opts,'lambda');
@@ -85,6 +87,7 @@ function results = main()
 			fprintf('% 1.3f% 1.3f\n',err(ii,cc),dp(ii,cc));
 			
 			% Indicate which voxels were used/update the set of unused voxels.
+            UNUSED_VOXELS(:,cc,ii+1) = UNUSED_VOXELS(:,cc,ii);
 			UNUSED_VOXELS(UNUSED_VOXELS(:,cc,ii),cc,ii+1) = fitObj(ii,cc).beta==0;
 
 			% Save a checkpoint file
@@ -95,7 +98,7 @@ function results = main()
         %% Test if the dprime is significantly greater than zero.
 		h = ttest(dp(ii,:),0,'Alpha',0.05,'Tail','right');
 		if isnan(h)
-			h = false
+			h = false;
 		end
         if h==true
             % If it is, reset jj ...
@@ -116,25 +119,57 @@ function results = main()
 
 	%% Fit model using all voxels from models with above chance performance (1:(ii-3)).
 	USEFUL_VOXELS = ~UNUSED_VOXELS(:,:,ii-3);
-	b=glmfit(Xtrain(:,USEFUL_VOXELS),Ytrain,'binomial');
-	a0 = b(1);
-	b(1) = [];
-	
-	% Evaluate this new model on the holdout set.
-	% Step 1: compute the model predictions.
-	yhat = X(:,USEFUL_VOXELS)*b + a0;
-	% Step 2: compute the error of those predictions.
-	errU = 1-mean(Y(FINAL_HOLDOUT)==(yhat(FINAL_HOLDOUT)>0));
-	% Step 3: compute the sensitivity of those predictions (dprime).
-	dpU = dprimeCV(Y,yhat>0,FINAL_HOLDOUT);
+    USEFUL_VOXELS = any(USEFUL_VOXELS,3);
 
+    opts.alpha = 0; % ridge regression
+    fitObj_ridge = init_glmnet_result_struct('glmnet',[1, N_CV]);
+    fitObj_cv_ridge = init_glmnet_result_struct('cvglmnet',[1, N_CV]);
+    for cc = start_cc:N_CV
+        disp(cc)
+		% Remove the holdout set
+		FINAL_HOLDOUT = CVBLOCKS(:,cc);
+        CV2 = CVBLOCKS(~FINAL_HOLDOUT,(1:N_CV)~=cc);
+		Xtrain = X(~FINAL_HOLDOUT,:);
+		Ytrain = Y(~FINAL_HOLDOUT);
+		        
+        % Convert CV2 to fold_id
+        fold_id = sum(bsxfun(@times,double(CV2),1:9),2);
+
+        % For some reason, this must be a row vector.
+        fold_id = transpose(fold_id);
+
+        % Run cvglmnet to determine a good lambda.
+        fitObj_cv_ridge(cc) = cvglmnet(Xtrain(:,USEFUL_VOXELS),Ytrain, ...
+                                 'binomial',opts,'class',9,fold_id);
+
+        % Set that lambda in the opts structure, and fit a new model.
+        opts.lambda = fitObj_cv_ridge(cc).lambda_min;
+        fitObj_ridge(cc) = glmnet(Xtrain(:,USEFUL_VOXELS),Ytrain,'binomial',opts);
+
+        % Unset lambda, so next time around cvglmnet will look for lambda
+        % itself.
+        opts = rmfield(opts,'lambda');
+
+        % Evaluate this new model on the holdout set.
+        % Step 1: compute the model predictions.
+        yhat = (X(:,USEFUL_VOXELS)*fitObj_ridge(cc).beta)+fitObj(cc).a0;
+        % Step 2: compute the error of those predictions.
+        err_ridge(cc) = 1-mean(Y(FINAL_HOLDOUT)==(yhat(FINAL_HOLDOUT)>0));
+        % Step 3: compute the sensitivity of those predictions (dprime).
+        dp_ridge(cc) = dprimeCV(Y,yhat>0,FINAL_HOLDOUT);
+	end
 	%% Package results 
+	results.errU = err_ridge;
+	results.dpU = dp_ridge;
+    results.UNUSED_VOXELS = UNUSED_VOXELS;
+    results.fitObj_ridge = fitObj_ridge;
+    results.fitObj_cv_ridge = fitObj_ridge_cv;
     results.fitObj = fitObj(1:ii,:);
     results.fitObj_cv = fitObj_cv(1:ii,:);
     results.err = err(1:ii,:);
     results.dp = dp(1:ii,:);
-	results.errU = errU;
-	results.dpU = dpU;
+	results.errU = err_ridge;
+	results.dpU = dp_ridge;
 	results.UNUSED_VOXELS = UNUSED_VOXELS(:,:,1:ii);
     delete('CHECKPOINT.mat');
 end
